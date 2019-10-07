af947de67d40dd89b2fbbb1d4375d00ba8d74ed4
thebinarylife <tbl@binarylife.co>
2019-10-07 11:49:42 -0400

    Add project files.

---
+++ HelloWorldGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
